97dd997f92744bbc11bd5a203da8b501043af090
Ben Morgan <bmorgan.warwick@gmail.com>
2012-05-18 15:57:20 +0100

    Add basic layout and first C++ application.

---
+++ Source/Resourceful.cpp
@@ -0,0 +1,25 @@
+// Resourceful - access application resource files using C++
+//
+//----------------------------------------------------------------------
+// Copyright (c) 2012, Ben Morgan <bmorgan.warwick@gmail.com>
+// Copyright (c) 2012, University of Warwick
+//
+// Distributed under the OSI-approved BSD 3-Clause License (the "License");
+// see accompanying file License.txt for details.
+//
+// This software is distributed WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the License for more information.
+//----------------------------------------------------------------------
+
+#include <iostream>
+
+int main() {
+  std::cout << "[Resourceful::Info] Hello World!" << std::endl;
+  return 0;
+}
+
+
+
+
+
